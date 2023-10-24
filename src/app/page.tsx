95a4669caa5d6b58bd3c6f53fec7fb37c47d03ca
qingnan.yqn <qingnan.yqn@alibaba-inc.com>
2023-10-24 17:58:18 +0800

    feat[typography]: add new typography sys

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@ import { Mdx } from '@/components/mdx';
 
 export default function RootPage() {
   return (
-    <div>
+    <article className='prose prose-slate dark:prose-invert'>
       <h4 className='text-lg'>This is <strong>Arno</strong>.</h4>
       <br className='inline-block p-4' />
       <ul>
-        <li>A software engineer currently working at <a className="hover:to-blue-600 from-blue-300 transition-all text-blue-600" href="https://www.dingtalk.com/">DingTalk</a> of Alibaba Inc.</li>
+        <li>A software engineer currently working at <a className="hover:to-blue-600 from-blue-300 transition-all text-blue-600" href="https://www.dingtalk.com/" target='_blank'>DingTalk</a> of Alibaba Inc.</li>
         <li>A designer of digital systems.</li>
         <li>A person try to be more creative during life time.</li>
       </ul>
-    </div>
+    </article>
   );
 };
